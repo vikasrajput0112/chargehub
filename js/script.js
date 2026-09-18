5d91de4edc8b0951eda56b9e5fff34d8606f345d
vikasrajput0112 <devops1@paisalo.in>
2026-09-18 11:45:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ function submitForm(event) {
     );
 
 }
+```javascript
+/* =========================================
+   COPY COUPON CODE
+========================================= */
+
+function copyCoupon(code, button) {
+
+    navigator.clipboard.writeText(code)
+        .then(function () {
+
+            const originalText = button.innerText;
+
+            button.innerText = "Copied!";
+
+            setTimeout(function () {
+
+                button.innerText = originalText;
+
+            }, 2000);
+
+        })
+        .catch(function () {
+
+            alert("Coupon Code: " + code);
+
+        });
+
+}
+```
+
